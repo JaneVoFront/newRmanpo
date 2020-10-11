59046db6e812e111c4e3dd11e8083902a467935a
Jane Vo <alterxru@gmail.com>
2020-10-12 01:45:30 +0300

    day 5

--- source/js/app.js
+++ source/js/app.js
@@ -18,12 +18,16 @@ console.log(x.getName())
 
 /*---кнопка закрытия блока---*/
 $(".close").on("click", function(){
- $('.close').closest('.show').removeClass('show');
+ $('.close').closest('.active').removeClass('active');
+});
+/*gen-menu_close*/
+$(".close").on("click", function(){
+    $('.close').siblings('.active').removeClass('active');
 });
 
 /*---search_popup__show---*/
 $(".search_icon").on("click", function(){
-  $('div.search-info').find('div.search_popup').addClass('show');
+  $('div.search-info').find('div.search_popup').addClass('active');
 });
 
 /*---svg-inline-to-HTML---*/
@@ -48,19 +52,49 @@ $('img.img-svg').each(function(){
 $(document).on("click", '.dropdown' , function(){
   $('.dropdown').find('ul').toggleClass("dropdown_open");
 });
-/*---выпадающее меню по клику---*/
-$(document).on("click", '.dropdown_tab' , function(){
-  $('.dropdown_tab').closest('ul').toggleClass("dropdown_open");
+
+$(document).on('click' , function(e){
+    if(!$(e.target).closest('body').length){
+        $('.dropdown ul').removeClass("dropdown_open");
+    }
 });
+
+/*---выпадающее меню по клику---*/
+(function($) {
+    $(function() {
+        $("ul.tabs__nav").on("click", "li:not(.active)", function() {
+            $(this)
+                .addClass("active")
+                .siblings()
+                .removeClass("active")
+                .closest("nav.tabs")
+                .find("div.tab__pane")
+                .removeClass("active")
+                .eq($(this).index())
+                .addClass("active")
+                .closest("nav.tabs")
+                .find("button.close")
+                .addClass("active");
+        });
+        $("ul.tabs__nav").on("click", "li.active",function () {
+            $(this)
+                .removeClass("active")
+                .closest("nav.tabs")
+                .find("div.tab__pane")
+                .eq($(this).index())
+                .removeClass("active")
+                .closest("nav.tabs")
+                .find("button.close")
+                .removeClass("active");
+
+        })
+    });
+})(jQuery);
 
 /*$('.dropdown').html('<div class="dropdown">'+$('.dropdown').html()+'</div>');*/
 
-$(document).on('click' , function(e){
-  if(!$(e.target).closest('body').length){
-    $('.dropdown ul').removeClass("dropdown_open");
-  }
-});
-/*---tabs для основного меню---*/
+
+/*---tabs для основного меню---
 $('.tab_content').hide();
 $('.tabs.animated-fade .tab-links .link_data a').on('click', function(e)  {
   var currentAttrValue = $(this).attr('href');
@@ -72,68 +106,4 @@ $('.tabs.animated-fade .tab-links .link_data a').on('click', function(e)  {
   $(this).parent('li').addClass('active').siblings().removeClass('active');
 
   e.preventDefault();
-});
-
-/*2/*/
-var $tabs = function (target) {
-  var
-      _elemTabs = (typeof target === 'string' ? document.querySelector(target) : target),
-      _eventTabsShow,
-      _showTab = function (tabsLinkTarget) {
-        var tabsPaneTarget, tabsLinkActive, tabsPaneShow;
-        tabsPaneTarget = document.querySelector(tabsLinkTarget.getAttribute('href'));
-        tabsLinkActive = tabsLinkTarget.parentElement.querySelector('.tabs__link_active');
-        tabsPaneShow = tabsPaneTarget.parentElement.querySelector('.tabs__pane_show');
-        // если следующая вкладка равна активной, то завершаем работу
-        if (tabsLinkTarget === tabsLinkActive) {
-          return;
-        }
-        // удаляем классы у текущих активных элементов
-        if (tabsLinkActive !== null) {
-          tabsLinkActive.classList.remove('tabs__link_active');
-        }
-        if (tabsPaneShow !== null) {
-          tabsPaneShow.classList.remove('tabs__pane_show');
-        }
-        // добавляем классы к элементам (в завимости от выбранной вкладки)
-        tabsLinkTarget.classList.add('tabs__link_active');
-        tabsPaneTarget.classList.add('tabs__pane_show');
-        document.dispatchEvent(_eventTabsShow);
-      },
-      _switchTabTo = function (tabsLinkIndex) {
-        var tabsLinks = _elemTabs.querySelectorAll('.tabs__link');
-        if (tabsLinks.length > 0) {
-          if (tabsLinkIndex > tabsLinks.length) {
-            tabsLinkIndex = tabsLinks.length;
-          } else if (tabsLinkIndex < 1) {
-            tabsLinkIndex = 1;
-          }
-          _showTab(tabsLinks[tabsLinkIndex - 1]);
-        }
-      };
-
-  _eventTabsShow = new CustomEvent('tab.show', { detail: _elemTabs });
-
-  _elemTabs.addEventListener('click', function (e) {
-    var tabsLinkTarget = e.target;
-    // завершаем выполнение функции, если кликнули не по ссылке
-    if (!tabsLinkTarget.classList.contains('tabs__link')) {
-      return;
-    }
-    // отменяем стандартное действие
-    e.preventDefault();
-    _showTab(tabsLinkTarget);
-  });
-
-  return {
-    showTab: function (target) {
-      _showTab(target);
-    },
-    switchTabTo: function (index) {
-      _switchTabTo(index);
-    }
-  }
-
-};
-
-$tabs('.tabs');
+});*/
--- source/js/search_popup.js
+++ source/js/search_popup.js
@@ -1,5 +1,5 @@
 'use strict'
 
 $(".search_icon").on("click", function(){
-    $('div.search-info').find('div.search_popup').addClass('search_popup__show show');
+    $('div.search-info').find('div.search_popup').addClass('active');
 });
